b4d8b31cbe7313608c171c95b68bba7515e2bfcb
Alberto Vieira de Lima <albertovieiradelima@gmail.com>
2021-11-10 15:29:45 -0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$AWS_REGION" ]] || [[ -z "$AWS_ACCESS_KEY_ID" ]] || [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo "Ensure that all environmental variables (AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY) are set!"
+  exit 1
+fi
+
+if [[ -z "$INPUT_SSM_PARAMETER_LIST" ]]; then
+  echo "Set SSM parameter name list (parameter_name_list) value(s)."
+  exit 1
+fi
+
+region="$AWS_REGION"
+parameter_name_list="$INPUT_SSM_PARAMETER_LIST"
+prefix="${INPUT_PREFIX:-AWS_SSM_}"
+ssm_start_prefix="$INPUT_SSM_START_PREFIX"
+jq_filter="$INPUT_JQ_FILTER"
+simple_json="$INPUT_SIMPLE_JSON"
+
+format_var_name () {
+  echo "$1" | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]"
+}
+
+get_ssm_param() {
+  parameter_name="$1"
+  ssm_param=$(aws --region "$region" ssm get-parameter --name "$parameter_name")
+  if [ -n "$jq_filter" ] || [ -n "$simple_json" ]; then
+    ssm_param_value=$(echo "$ssm_param" | jq '.Parameter.Value | fromjson')
+    if [ -n "$simple_json" ] && [ "$simple_json" == "true" ]; then
+      for p in $(echo "$ssm_param_value" | jq -r --arg v "$prefix" 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' ); do
+        IFS='=' read -r var_name var_value <<< "$p"
+        echo "$(format_var_name "$var_name")=$var_value" >> $GITHUB_ENV
+      done
+    else
+      IFS=' ' read -r -a params <<< "$jq_filter"
+      for var_name in "${params[@]}"; do
+        var_value=$(echo "$ssm_param_value" | jq -r -c "$var_name")
+        echo "$(format_var_name "$var_name")=$var_value" >> $GITHUB_ENV
+      done
+    fi
+  else
+    var_name=$(echo "$ssm_param" | jq -r '.Parameter.Name' | awk -F/ '{print $NF}')
+    var_value=$(echo "$ssm_param" | jq -r '.Parameter.Value')
+    var_name_parsed=${var_name:${#ssm_start_prefix}}
+    echo "$(format_var_name "$var_name_parsed")=$var_value" >> $GITHUB_ENV
+  fi
+}
+
+for parameter in $(echo $parameter_name_list | sed "s/,/ /g"); do
+  get_ssm_param "${ssm_start_prefix}$parameter"
+done
+
